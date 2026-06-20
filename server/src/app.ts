9255a1afb0cef2837a4dae8cad994dd99514d4aa
Jay <jayjthakkar13@gmail.com>
2026-06-20 16:03:44 +0530

    add mongoose and mongoDB connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,25 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config";
 
 const app = express();
+const PORT = process.env.PORT ?? 5000;
+
+dotenv.config();
+
 app.use(cors());
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {
 	res.json({ status: 'OK' });
 });
+
+connectDB()
+  .then(() => {
+    console.log('MongoDB is connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err: Error) => console.error('MongoDB Connection Error', err));
 
 export default app;
